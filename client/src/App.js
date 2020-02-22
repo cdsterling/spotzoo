033436835e49e29a1d9a9744327a3dc58c54672a
Chad Sterling <cdsterling@gmail.com>
2020-02-22 12:40:47 -0800

    adding local env file as well as mapbox-gl

--- client/src/App.js
+++ client/src/App.js
@@ -200,80 +200,58 @@ class App extends Component {
               />
             )
           )} */}
-
           <SmallCard 
             emoji={'🐇'}
             name={'name'}
             timestamp={'timestamp'}
             submitted_by={'submitted by'}
           />
-          <SmallCard 
-            emoji={'🐇'}
-            name={'name'}
-            timestamp={'timestamp'}
-            submitted_by={'submitted by'}
-          />
-          <SmallCard 
-            emoji={'🐇'}
-            name={'name'}
-            timestamp={'timestamp'}
-            submitted_by={'submitted by'}
-          />
-          <SmallCard 
-            emoji={'🐇'}
-            name={'name'}
-            timestamp={'timestamp'}
-            submitted_by={'submitted by'}
-          />
-          <SmallCard 
-            emoji={'🐇'}
-            name={'name'}
-            timestamp={'timestamp'}
-            submitted_by={'submitted by'}
-          />
+          
         </div>
+
         <div className="MapContainer">
-        <ReactMapGl
-        {...this.state.viewport}
-        mapboxApiAccessToken = {process.env.REACT_APP_TOKEN}
-        mapStyle ='mapbox://styles/marby87/ck6j39qkz0i7k1inu9gqqc4o1'
-        onViewportChange={(viewport) => this.onViewportChange(viewport)}> 
-        
-        {Object.keys(this.state.userLocation).length !== 0 ? (
-          <Marker
-            className="user"
-            keys ="1"
-            latitude={this.state.userLocation.lat}
-            longitude={this.state.userLocation.long}
-          >
-          
-            <img className = "location-icon" alt="location-icon" src={User}/>
-          </Marker>
-        ) : ( 
-           <div>Empty</div>
-        )}
-
-        {Object.values(this.state.data).length !==0 ?(
-          this.state.data.map((data,index) => (
-          <Marker
-            className = "markers"
-            keys={data._id}
-            id={data._id}
-            latitude={data.latitude}
-            longitude={data.longitude}
             
-            >
-            <img className = "location-icon" alt="location-icon" src={Red} 
-            />
-            {this.state.distance[data._id]} <br/> {data.animal}  </Marker>
-  
-          ))
-        ) : (
-          <div>Empty</div>
-        )}
-          
+            <ReactMapGl
+                {...this.state.viewport}
+                mapboxApiAccessToken = {process.env.REACT_APP_TOKEN}
+                mapStyle ='mapbox://styles/marby87/ck6j39qkz0i7k1inu9gqqc4o1'
+                onViewportChange={(viewport) => this.onViewportChange(viewport)}> 
+                
+                {Object.keys(this.state.userLocation).length !== 0 ? (
+                <Marker
+                    className="user"
+                    keys ="1"
+                    latitude={this.state.userLocation.lat}
+                    longitude={this.state.userLocation.long}
+                >
+            
+                    <img className = "location-icon" alt="location-icon" src={User}/>
+                </Marker>
+                ) : ( 
+                <div>Empty</div>
+                )}
 
-      </ReactMapGl>
+                {Object.values(this.state.data).length !==0 ?(
+                this.state.data.map((data,index) => (
+                <Marker
+                    className = "markers"
+                    keys={data._id}
+                    id={data._id}
+                    latitude={data.latitude}
+                    longitude={data.longitude}
+                    
+                    >
+                    <img className = "location-icon" alt="location-icon" src={Red} 
+                    />
+                    {this.state.distance[data._id]} <br/> {data.animal}  </Marker>
+        
+                ))
+                ) : (
+                <div>Empty</div>
+                )}
+            
+
+            </ReactMapGl>
         </div>
       </div>
     );
