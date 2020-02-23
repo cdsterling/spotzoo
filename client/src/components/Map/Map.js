b54acfe79c406b48843df33a316a69e11f810229
Chad Sterling <cdsterling@gmail.com>
2020-02-22 20:37:57 -0800

    adding boilerplate for new components and pages

--- client/src/components/Map/Map.js
+++ client/src/components/Map/Map.js
@@ -1,1 +1,11 @@
 // map.js
+import React, { Component } from 'react';
+
+
+import './Map.css';
+class Map extends Component {
+
+
+
+}
+export default NavBar;
--- client/src/components/pages/Home/Home.js
+++ client/src/components/pages/Home/Home.js
@@ -1,1 +1,11 @@
 // Home
+import React, { Component } from 'react';
+
+
+import './Home.css';
+class Home extends Component {
+
+
+
+}
+export default Home;
